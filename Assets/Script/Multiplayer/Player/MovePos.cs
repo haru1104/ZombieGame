888960eb7c350d478d327a3d63f1017630b048d5
stageroad0820 <stageroad0820@naver.com>
2021-09-02 15:01:38 +0900

    빌드 전 수정된 내용 - 싱글 플레이 버튼 제거 - 터치패드로 움직이도록 수정

--- Assets/Script/Multiplayer/Player/MovePos.cs
+++ Assets/Script/Multiplayer/Player/MovePos.cs
@@ -8,14 +8,14 @@ namespace haruroad.szd.multiplayer {
         private Animator playerAni;
         private Rigidbody playerRigid;
         private Vector3 movePos;
+
+        private PlayerHP hp;
+
         private float h, v;
         private float TurnSpeed = 2;
-        private PlayerHP hp;
+        
         [SerializeField]
         private float moveSpeed = 12f;
-        //private float jumpPos;
-        //private bool isJump;
-        //private bool isWalk;
 
         void Start() {
             StartReset();
@@ -29,33 +29,31 @@ namespace haruroad.szd.multiplayer {
             playerRigid = GetComponent<Rigidbody>();
             playerAni = GetComponent<Animator>();
             hp = GetComponent<PlayerHP>();
-            //jumpPos = 5;
-            //isJump = false;
-            //isWalk = false;
 
         }
         private void MovePosSet() {
-            if (photonView.IsMine == false && PhotonNetwork.IsConnected == true || hp.health <= 0) {
+            if (!photonView.IsMine && PhotonNetwork.IsConnected || hp.health <= 0) {
                 return;
             }
 
-            h = Input.GetAxis("Horizontal");
-            v = Input.GetAxis("Vertical");
+            // h = Input.GetAxis("Horizontal");
+            // v = Input.GetAxis("Vertical");
 
             movePos = v * transform.forward;
             movePos = movePos.normalized * moveSpeed * Time.deltaTime;
+
             playerRigid.MovePosition(transform.position + movePos);
             playerRigid.rotation = playerRigid.rotation * Quaternion.Euler(0, h * TurnSpeed, 0);
+
             if (h == 0 && v == 0) {
                 playerAni.SetBool("Walk", false);
-                //isWalk = false;
             }
             else {
-                //isWalk = true;
                 playerAni.SetBool("Walk", true);
             }
 
         }
+
         public void OnStickChanged(Vector3 stickPos) {
             h = stickPos.x;
             v = stickPos.y;
